e90a2e1e091d8d0ef1370e728063bbad7c2e7d43
Mathias Fredriksson <mafredri@gmail.com>
2023-02-10 17:54:34 +0200

    fix: do first try immediately, then activate floor (#27)

--- retrier_test.go
+++ retrier_test.go
@@ -15,3 +15,17 @@ func TestContextCancel(t *testing.T) {
 		t.Fatalf("attempt allowed even though context cancelled")
 	}
 }
+
+func TestFirstTryImmediately(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	r := New(time.Hour, time.Hour)
+	tt := time.Now()
+	if !r.Wait(ctx) {
+		t.Fatalf("attempt not allowed")
+	}
+	if time.Since(tt) > time.Second {
+		t.Fatalf("attempt took too long")
+	}
+}
